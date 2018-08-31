b3e3d25c9612377df9defe7691b7e1d4a45c1914
czy211 <czy211@hotmail.com>
2018-08-31 15:25:42 +0800

    支持顯示靈魂裂片碎塊數量

--- My_SoulShard/My_SoulShard.lua
+++ My_SoulShard/My_SoulShard.lua
@@ -5,12 +5,28 @@ ss:RegisterEvent("UNIT_POWER_UPDATE")
 
 ss.shards = {}
 
+-- 靈魂裂片碎塊文字
+ss.text = ss:CreateFontString("Slivers", "Overlay")
+ss.text:SetFont("Fonts\\ARHei.ttf", 24)
+ss.text:SetTextColor(1, 0, 0)
+
+-- 靈魂裂片大小
+local size = 36
+
 function ss:Update()
-    -- 7：靈魂裂片
-    local available = UnitPower("player", 7)
+    local numPower = WarlockPowerBar_UnitPower("player")
+    local numShards, numSlivers = math.modf(numPower)
     for i = 1, #ss.shards do
-        local alpha = i > available and 0.15 or 1
+        local alpha = i > numShards and 0.15 or 1
         ss.shards[i]:SetAlpha(alpha)
+
+        -- 顯示靈魂裂片碎塊數量
+        if i == numShards + 1 and numSlivers > 0 then
+            ss.text:SetText(numSlivers * 10)
+            ss.text:SetPoint("Center", ss, "Left", size * (numShards + 0.5), 0)
+        elseif numSlivers == 0 then
+            ss.text:SetText("")
+        end
     end
 end
 
@@ -22,19 +38,17 @@ ss:SetScript("OnEvent", function(self, event, unit, ...)
         spec = spec and GetSpecializationInfo(spec) or nil
         -- 265:痛苦，266：惡魔，267：毀滅
         if spec == 265 or spec == 266 or spec == 267 then
-            -- 7：靈魂裂片
-            local numPower = UnitPowerMax("player", 7)
-            local size = 36
+            local maxNumPower = UnitPowerMax("player", Enum.PowerType.SoulShards)
 
             if ss:GetHeight() == 0 then
                 ss:SetHeight(size)
-                ss:SetWidth(size * numPower)
+                ss:SetWidth(size * maxNumPower)
                 ss:SetPoint("Top", UIParent, "Bottom", 0, 240)
             end
             ss:Show()
 
             if next(ss.shards) == nil then
-                for i = 1, numPower do
+                for i = 1, maxNumPower do
                     local shard = ss:CreateTexture(nil, "Artwork")
                     shard:SetTexture("Interface\\ICONS\\INV_Misc_Gem_Amethyst_02")
                     shard:SetWidth(size)
