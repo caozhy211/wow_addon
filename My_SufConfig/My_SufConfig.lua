4bffc6d0cfb4500935853c4a78072d8ee1d6132d
czy211 <czy211@hotmail.com>
2018-09-12 18:36:24 +0800

    SUF設置介面現在不需要重載UI就會顯示通過點擊加入黑名單的光環

--- My_SufConfig/My_SufConfig.lua
+++ My_SufConfig/My_SufConfig.lua
@@ -19,16 +19,9 @@ f:SetScript("OnEvent", function(self, event)
         -- 左鍵點擊光環加入黑名單
         local auras = ShadowUF.modules.auras
 
-        local function reloadUnitAuras()
-            for _, frame in pairs(ShadowUF.Units.unitFrames) do
-                if (UnitExists(frame.unit) and frame.visibility.auras) then
-                    auras:UpdateFilter(frame)
-                    frame:FullUpdate()
-                end
-            end
-        end
-
+        -- 因爲解鎖框架時，會調用setfenv函數改變Update函數的環境，所以不能使用hooksecurefunc函數
         auras.Update = function(self, frame)
+            -- Update函數的原內容
             local config = ShadowUF.db.profile.units[frame.unitType].auras
             if frame.auras.anchor then
                 frame.auras.anchor.totalAuras = frame.auras.anchor.temporaryEnchants
@@ -56,6 +49,67 @@ f:SetScript("OnEvent", function(self, event)
                 end
             end
 
+            -- 重新加載光環
+            local function ReloadUnitAuras()
+                for _, unitFrame in pairs(ShadowUF.Units.unitFrames) do
+                    if (UnitExists(unitFrame.unit) and unitFrame.visibility.auras) then
+                        auras:UpdateFilter(unitFrame)
+                        unitFrame:FullUpdate()
+                    end
+                end
+            end
+
+            -- 更新黑名單
+            local function UpdateBlackLists()
+                if ShadowUF.Config and ShadowUF.Config.options then
+                    local blacklists = ShadowUF.Config.options.args.filter.args.filters.args.blacklists
+                    local filterIndex = 0
+                    local spellIndex = 0
+
+                    for filter, spells in pairs(ShadowUF.db.profile.filters["blacklists"]) do
+                        filterIndex = filterIndex + 1
+                        blacklists.args[tostring(filterIndex)].args.spells.args = {}
+
+                        local hasSpell
+                        for spellName in pairs(spells) do
+                            if spellName ~= "buffs" and spellName ~= "debuffs" then
+                                hasSpell = true
+                                spellIndex = spellIndex + 1
+                                blacklists.args[tostring(filterIndex)].args.spells.args[spellIndex .. "label"] = {
+                                    order = spellIndex,
+                                    type = "description",
+                                    width = "double",
+                                    fontSize = "medium",
+                                    name = spellName,
+                                }
+                                blacklists.args[tostring(filterIndex)].args.spells.args[tostring(spellIndex)] = {
+                                    order = spellIndex + 0.5,
+                                    type = "execute",
+                                    name = ShadowUF.L["Delete"],
+                                    width = "half",
+                                    func = function(info)
+                                        ShadowUF.db.profile.filters["blacklists"][filter][spellName] = nil
+
+                                        ReloadUnitAuras()
+                                        UpdateBlackLists()
+                                    end
+                                }
+                            end
+                        end
+
+                        if not hasSpell then
+                            blacklists.args[tostring(filterIndex)].args.spells.args.noSpells = {
+                                order = 0,
+                                type = "description",
+                                name = ShadowUF.L["This filter has no auras in it, you will have to add some using the "
+                                        .. "dialog above."],
+                            }
+                        end
+                    end
+                end
+            end
+
+            -- 點擊增益光環
             if frame.auras.buffs then
                 local buttons = frame.auras.buffs.buttons
                 for _, button in pairs(buttons) do
@@ -64,7 +118,8 @@ f:SetScript("OnEvent", function(self, event)
                         if mouse == "LeftButton" then
                             local value = UnitAura(self.unit, self.auraID, self.filter)
                             ShadowUF.db.profile.filters["blacklists"]["增益"][value] = true
-                            reloadUnitAuras()
+                            ReloadUnitAuras()
+                            UpdateBlackLists()
                         elseif InCombatLockdown() or self.filter == "TEMP" or (not UnitIsUnit(self.parent.unit, "player")
                                 and not UnitIsUnit(self.parent.unit, "vehicle")) then
                             return
@@ -75,6 +130,7 @@ f:SetScript("OnEvent", function(self, event)
                 end
             end
 
+            -- 點擊減益光環
             if frame.auras.debuffs then
                 local buttons = frame.auras.debuffs.buttons
                 for _, button in pairs(buttons) do
@@ -82,10 +138,12 @@ f:SetScript("OnEvent", function(self, event)
                     button:SetScript("OnClick", function(self, mouse)
                         local value = UnitAura(self.unit, self.auraID, self.filter)
                         ShadowUF.db.profile.filters["blacklists"]["減益"][value] = true
-                        reloadUnitAuras()
+                        ReloadUnitAuras()
+                        UpdateBlackLists()
                     end)
                 end
             end
         end
     end
 end)
+
