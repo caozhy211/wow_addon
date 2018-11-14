bac3d00cc30b767f6ee7a47ee985c468c6ca6151
czy211 <czy211@hotmail.com>
2018-11-14 18:40:08 +0800

    1.Memory重命名爲MemoryBar 2.MemoryBar背景調整爲黑色，設置透明度 3.最大值和最小值移至MemoryBar上

--- MyAddOn/MemoryBar.lua
+++ MyAddOn/MemoryBar.lua
@@ -1,11 +1,20 @@
+local font = GameFontNormal:GetFont()
 local addons = {}
 local bar = CreateFrame("StatusBar", "MyMemoryBar", UIParent)
 bar:SetSize(298 - 119 - 47, 11)
 bar:SetPoint("BottomRight", -47, 88 - 11)
 bar:SetBackdrop({ bgFile = "Interface\\ChatFrame\\ChatFrameBackground" })
-bar:SetBackdropColor(1, 0, 0)
+bar:SetBackdropColor(0, 0, 0, 0.8)
 bar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
 bar:SetStatusBarColor(1, 1, 0)
+
+local left = bar:CreateFontString()
+left:SetFont(font, 10, "Outline")
+left:SetPoint("Left", 2, 0)
+
+local right = bar:CreateFontString()
+right:SetFont(font, 10, "Outline")
+right:SetPoint("Right", -2, 0)
 
 bar:RegisterEvent("PLAYER_LOGIN")
 
@@ -21,50 +30,13 @@ bar:SetScript("OnEvent", function()
     end
 end)
 
-local function FormatMemory(memory, type)
-    local color = type == "min" and "|cff00ff00min: " or type == "max" and "|cffff0000max: " or "|cffffff00"
-    if memory < 1000 then
-        return format(color .. "%.2fKB|r", memory)
-    end
-    return format(color .. "%.2fMB|r", memory / 1000)
-end
-
-bar:SetScript("OnUpdate", function(self, elapsed)
-    self.elapsed = (self.elapsed or 0) + elapsed
-    if self.elapsed < 1 then
-        return
-    end
-    self.elapsed = 0
-
-    UpdateAddOnMemoryUsage()
-    self.minTotal = 0
-    self.currentTotal = 0
-    self.maxTotal = 0
-    for i = 1, #addons do
-        local name = addons[i].name
-        local memory = GetAddOnMemoryUsage(name)
-        if memory < addons[i].minMemory then
-            addons[i].minMemory = memory
-        end
-        if memory > addons[i].maxMemory then
-            addons[i].maxMemory = memory
-        end
-        addons[i].memory = memory
-
-        self.minTotal = self.minTotal + addons[i].minMemory
-        self.currentTotal = self.currentTotal + addons[i].memory
-        self.maxTotal = self.maxTotal + addons[i].maxMemory
-    end
-    self:SetMinMaxValues(0, self.maxTotal)
-    self:SetValue(self.currentTotal)
-    collectgarbage()
-end)
-
 C_Timer.NewTicker(1, function()
     UpdateAddOnMemoryUsage()
+
     bar.minTotal = 0
     bar.currentTotal = 0
     bar.maxTotal = 0
+
     for i = 1, #addons do
         local name = addons[i].name
         local memory = GetAddOnMemoryUsage(name)
@@ -80,18 +52,29 @@ C_Timer.NewTicker(1, function()
         bar.currentTotal = bar.currentTotal + addons[i].memory
         bar.maxTotal = bar.maxTotal + addons[i].maxMemory
     end
+
     bar:SetMinMaxValues(0, bar.maxTotal)
     bar:SetValue(bar.currentTotal)
+
+    left:SetFormattedText("|cff00ff00%.2fMB|r", bar.minTotal / 1000)
+    right:SetFormattedText("|cffff0000%.2fMB|r", bar.maxTotal / 1000)
+
     collectgarbage()
 end)
+
+local function FormatMemory(memory, type)
+    local color = type == "min" and "|cff00ff00min: " or type == "max" and "|cffff0000max: " or "|cffffff00"
+    if memory < 1000 then
+        return format(color .. "%.2fKB|r", memory)
+    end
+    return format(color .. "%.2fMB|r", memory / 1000)
+end
 
 bar:SetScript("OnEnter", function(self)
     GameTooltip:SetOwner(self, "ANCHOR_NONE")
     GameTooltip:SetPoint("Bottom", self, "Top", 0, 2)
     GameTooltip:ClearLines()
     GameTooltip:AddLine(FormatMemory(self.currentTotal))
-    GameTooltip:AddLine(FormatMemory(self.minTotal, "min"))
-    GameTooltip:AddLine(FormatMemory(self.maxTotal, "max"))
     GameTooltip:Show()
     self:SetScript("OnUpdate", function(self, elapsed)
         self.elapsed = (self.elapsed or 0) + elapsed
@@ -100,8 +83,6 @@ bar:SetScript("OnEnter", function(self)
         end
         self.elapsed = 0
         _G["GameTooltipTextLeft1"]:SetText(FormatMemory(self.currentTotal))
-        _G["GameTooltipTextLeft2"]:SetText(FormatMemory(self.minTotal, "min"))
-        _G["GameTooltipTextLeft3"]:SetText(FormatMemory(self.maxTotal, "max"))
     end)
 end)
 
