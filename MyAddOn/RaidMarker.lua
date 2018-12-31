02964437e870dac2b89f18c743e29f390260e6d0
czy211 <czy211@hotmail.com>
2018-12-31 23:31:55 +0800

    標記圖示修改爲一個框架，左鍵標記當前目標，右鍵放置世界標記

--- MyAddOn/RaidMarker.lua
+++ MyAddOn/RaidMarker.lua
@@ -1,25 +1,13 @@
-local worldRaidMarkerColors = {
-    { 1, 1, 1 },
-    { 1, 0, 0 },
-    { 0, 0, 1 },
-    { 0.75, 0.75, 0.75 },
-    { 0, 1, 0 },
-    { 0.5, 0, 0.5 },
-    { 1, 0.65, 0 },
-    { 1, 1, 0 },
-    { 0, 0, 0 },
-}
-
 local worldRaidMarkerMacroValues = {
-    "/cwm 8\n/wm 8",
-    "/cwm 4\n/wm 4",
-    "/cwm 1\n/wm 1",
-    "/cwm 7\n/wm 7",
-    "/cwm 2\n/wm 2",
-    "/cwm 3\n/wm 3",
-    "/cwm 6\n/wm 6",
-    "/cwm 5\n/wm 5",
     "/cwm 0",
+    "/cwm 5\n/wm 5",
+    "/cwm 6\n/wm 6",
+    "/cwm 3\n/wm 3",
+    "/cwm 2\n/wm 2",
+    "/cwm 7\n/wm 7",
+    "/cwm 1\n/wm 1",
+    "/cwm 4\n/wm 4",
+    "/cwm 8\n/wm 8",
 }
 
 local raidMarkers = CreateFrame("Frame", "MyRaidMarkerFrame", UIParent)
@@ -29,14 +17,6 @@ raidMarkers:SetBackdrop({ bgFile = "Interface\\ChatFrame\\ChatFrameBackground" }
 raidMarkers:SetBackdropColor(0, 0, 0, 0.2)
 raidMarkers:SetAlpha(0)
 
-local worldRaidMarkers = CreateFrame("Frame", "MyWorldRaidMarkerFrame", UIParent)
-worldRaidMarkers:SetSize(240, 24)
-worldRaidMarkers:SetPoint("BottomLeft", 1380 + 1, 257 + 1)
-worldRaidMarkers:SetBackdrop({ bgFile = "Interface\\ChatFrame\\ChatFrameBackground" })
-worldRaidMarkers:SetBackdropColor(0, 0, 0, 0.2)
-worldRaidMarkers:SetAlpha(0)
-worldRaidMarkers:SetFrameStrata("Low")
-
 raidMarkers:SetScript("OnEnter", function(self)
     self:SetAlpha(1)
 end)
@@ -45,29 +25,8 @@ raidMarkers:SetScript("OnLeave", function(self)
     self:SetAlpha(0)
 end)
 
-raidMarkers:SetScript("OnMouseWheel", function(self)
-    if not UnitInParty("player") and not UnitInRaid("player") then
-        return
-    end
-    self:Hide()
-    worldRaidMarkers:SetAlpha(1)
-end)
-
-worldRaidMarkers:SetScript("OnEnter", function(self)
-    self:SetAlpha(1)
-end)
-
-worldRaidMarkers:SetScript("OnLeave", function(self)
-    self:SetAlpha(0)
-end)
-
-worldRaidMarkers:SetScript("OnMouseWheel", function(self)
-    self:SetAlpha(0)
-    raidMarkers:Show()
-end)
-
 local function CreateRaidMarkerButton(id)
-    local button = CreateFrame("Button", nil, raidMarkers)
+    local button = CreateFrame("Button", nil, raidMarkers, "SecureActionButtonTemplate")
     button:SetSize(24, 24)
     button:SetPoint("Left", (NUM_RAID_ICONS - id) * (24 + 3) + 1, 0)
 
@@ -82,6 +41,9 @@ local function CreateRaidMarkerButton(id)
         button:GetNormalTexture():SetTexCoord(left, right, top, bottom)
     end
 
+    button:SetAttribute("type2", "macro")
+    button:SetAttribute("macrotext", worldRaidMarkerMacroValues[id + 1])
+
     button:SetScript("OnEnter", function()
         raidMarkers:SetAlpha(1)
     end)
@@ -90,9 +52,12 @@ local function CreateRaidMarkerButton(id)
         raidMarkers:SetAlpha(0)
     end)
 
-    button:RegisterForClicks("LeftButtonUp")
-    button:SetScript("OnClick", function()
-        SetRaidTarget("target", id)
+    button:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+
+    button:HookScript("OnClick", function(_, mouse)
+        if mouse == "LeftButton" then
+            SetRaidTarget("target", id)
+        end
     end)
 
     return button
@@ -100,28 +65,4 @@ end
 
 for i = 0, NUM_RAID_ICONS do
     CreateRaidMarkerButton(i)
-end
-
-local function CreateWorldRaidMarkerButton(id)
-    local button = CreateFrame("Button", nil, worldRaidMarkers, "SecureActionButtonTemplate")
-    button:SetSize(24, 24)
-    button:SetPoint("Left", id * (24 + 3) + 1, 0)
-
-    button:SetNormalTexture("Interface\\ChatFrame\\ChatFrameBackground")
-    button:GetNormalTexture():SetColorTexture(unpack(worldRaidMarkerColors[id + 1]))
-
-    button:SetAttribute("type1", "macro")
-    button:SetAttribute("macrotext", worldRaidMarkerMacroValues[id + 1])
-
-    button:SetScript("OnEnter", function()
-        worldRaidMarkers:SetAlpha(1)
-    end)
-
-    button:SetScript("OnLeave", function()
-        worldRaidMarkers:SetAlpha(0)
-    end)
-end
-
-for i = 0, NUM_WORLD_RAID_MARKERS do
-    CreateWorldRaidMarkerButton(i)
 end
