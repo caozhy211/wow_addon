20b95a57e61a20a621c8a6e5f5869554ce15034d
czy211 <czy211@hotmail.com>
2019-01-03 12:47:53 +0800

    修復聊天框其他人獲得的物品等級顯示不正確的問題

--- MyAddOn/ItemInfo.lua
+++ MyAddOn/ItemInfo.lua
@@ -448,7 +448,7 @@ local function ChatItemInfo(hyperlink)
 
     local link = strmatch(hyperlink, "|H(.-)|h")
     local name, _, _, _, _, class, subclass, _, equipSlot = GetItemInfo(link)
-    local level = GetDetailedItemLevelInfo(link)
+    local level = ScanItemTooltip(link)
     local checkGem = true
     if level then
         if equipSlot and strfind(equipSlot, "INVTYPE_") then
