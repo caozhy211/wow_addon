df1484009c145e2e8e7a0c0a08bf974bdb7b31da
czy211 <czy211@hotmail.com>
2020-01-13 14:21:16 +0800

    修复首次登录不显示灵魂碎片的问题
    因为首次登录使用 UnitPowerMax 方法获取最大碎片数是 0

--- src/SoulShards.lua
+++ src/SoulShards.lua
@@ -1,70 +1,82 @@
 local _, class = UnitClass("player")
 if class == "WARLOCK" then
     ---@type Frame
-    local shardFrame = CreateFrame("Frame", "WLK_SoulShardFrame", UIParent)
-    shardFrame:SetSize(228, 20)
-    shardFrame:SetPoint("BOTTOM", 0, 185)
-    shardFrame:SetAlpha(0)
+    local shardPane = CreateFrame("Frame", "WLK_SoulShardPane", UIParent)
+    shardPane:SetSize(228, 20)
+    shardPane:SetPoint("BOTTOM", 0, 185)
 
     ---@type FontString 灵魂碎片裂片标签
-    local partialLabel = shardFrame:CreateFontString(nil, "ARTWORK", "Game15Font_o1")
+    local partialLabel = shardPane:CreateFontString(nil, "ARTWORK", "Game15Font_o1")
     partialLabel:SetTextColor(GetTableColor(YELLOW_FONT_COLOR))
 
-    local maxShards = UnitPowerMax("player", Enum.PowerType.SoulShards)
-    local shardWidth = shardFrame:GetHeight() * 2
-    local spacing = floor((shardFrame:GetWidth() - maxShards * shardWidth) / (maxShards - 1))
     ---@type table<number, Frame>
     local shards = {}
-    for i = 1, maxShards do
-        ---@type Frame
-        local shard = CreateFrame("Frame", nil, shardFrame)
-        shard:SetSize(shardWidth, shardFrame:GetHeight())
-        shard:SetPoint("LEFT", (shardWidth + spacing) * (i - 1), 0)
-        shard:SetBackdrop({
-            edgeFile = "Interface/Buttons/WHITE8X8",
-            edgeSize = 2,
-        })
-        ---@type Texture
-        local background = shard:CreateTexture(nil, "BACKGROUND")
-        background:SetAllPoints()
-        background:SetTexture("Interface/ChatFrame/CHATFRAMEBACKGROUND")
-        background:SetColorTexture(GetClassColor(class))
-        shard.background = background
-        tinsert(shards, shard)
-    end
 
     if UnitLevel("player") < SHARDBAR_SHOW_LEVEL then
-        shardFrame:RegisterEvent("PLAYER_LEVEL_UP")
+        -- 等级低于显示灵魂碎片的等级时，隐藏碎片面板，并注册 PLAYER_LEVEL_UP 事件
+        shardPane:SetAlpha(0)
+        shardPane:RegisterEvent("PLAYER_LEVEL_UP")
     else
-        shardFrame:RegisterEvent("PLAYER_LOGIN")
-        shardFrame:RegisterUnitEvent("UNIT_POWER_FREQUENT", "player")
+        shardPane:RegisterEvent("PLAYER_LOGIN")
+        shardPane:RegisterUnitEvent("UNIT_POWER_FREQUENT", "player")
     end
 
-    shardFrame:SetScript("OnEvent", function(_, event, ...)
+    --- 创建灵魂碎片框架
+    local function CreateShardFrame()
+        local shardWidth = shardPane:GetHeight() * 2
+        local maxShards = UnitPowerMax("player", Enum.PowerType.SoulShards)
+        local spacing = floor((shardPane:GetWidth() - maxShards * shardWidth) / (maxShards - 1))
+        for i = 1, maxShards do
+            ---@type Frame
+            local shard = CreateFrame("Frame", nil, shardPane)
+            shard:SetSize(shardWidth, shardPane:GetHeight())
+            shard:SetPoint("LEFT", (shardWidth + spacing) * (i - 1), 0)
+            shard:SetBackdrop({
+                edgeFile = "Interface/Buttons/WHITE8X8",
+                edgeSize = 2,
+            })
+            ---@type Texture
+            local background = shard:CreateTexture(nil, "BACKGROUND")
+            background:SetAllPoints()
+            background:SetTexture("Interface/ChatFrame/CHATFRAMEBACKGROUND")
+            background:SetColorTexture(GetClassColor(class))
+            shard.background = background
+            tinsert(shards, shard)
+        end
+    end
+
+    --- 更新灵魂碎片
+    local function UpdateShards()
+        local power = WarlockPowerBar_UnitPower("player")
+        local numShards, numPartials = math.modf(power)
+        for i = 1, #shards do
+            shards[i].background:SetAlpha(i > numShards and 0 or 1)
+            -- 显示灵魂碎片裂片数量
+            if numPartials > 0 and i == numShards + 1 then
+                partialLabel:SetText(numPartials * 10)
+                partialLabel:SetPoint("CENTER", shards[i])
+            elseif numPartials == 0 then
+                partialLabel:SetText("")
+            end
+        end
+    end
+
+    shardPane:SetScript("OnEvent", function(_, event, ...)
         if event == "PLAYER_LEVEL_UP" then
             local level = ...
             if level >= SHARDBAR_SHOW_LEVEL then
-                shardFrame:UnregisterEvent(event)
-                shardFrame:RegisterUnitEvent("UNIT_POWER_FREQUENT", "player")
-                shardFrame:SetAlpha(1)
+                shardPane:UnregisterEvent(event)
+                shardPane:RegisterUnitEvent("UNIT_POWER_FREQUENT", "player")
+                shardPane:SetAlpha(1)
+                CreateShardFrame()
+                UpdateShards()
             end
-        elseif event == "PLAYER_LOGIN" or event == "UNIT_POWER_FREQUENT" then
-            if event == "PLAYER_LOGIN" then
-                shardFrame:UnregisterEvent(event)
-                shardFrame:SetAlpha(1)
-            end
-            local power = WarlockPowerBar_UnitPower("player")
-            local numShards, numPartials = math.modf(power)
-            for i = 1, #shards do
-                shards[i].background:SetAlpha(i > numShards and 0 or 1)
-                -- 显示灵魂碎片裂片数量
-                if numPartials > 0 and i == numShards + 1 then
-                    partialLabel:SetText(numPartials * 10)
-                    partialLabel:SetPoint("CENTER", shards[i])
-                elseif numPartials == 0 then
-                    partialLabel:SetText("")
-                end
-            end
+        elseif event == "PLAYER_LOGIN" then
+            shardPane:UnregisterEvent(event)
+            CreateShardFrame()
+            UpdateShards()
+        elseif event == "UNIT_POWER_FREQUENT" then
+            UpdateShards()
         end
     end)
 end
