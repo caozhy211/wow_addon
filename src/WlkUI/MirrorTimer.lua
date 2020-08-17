a528a6061e27c467a9353bf4f0eff385fb0abde7
czy211 <czy211@hotmail.com>
2020-08-17 20:41:59 +0800

    feat: 设置 MirrorTimer
    1. 隐藏 MirrorTimer 边框。
    2. 显示 MirrorTimer 时间。

---
+++ src/WlkUI/MirrorTimer.lua
@@ -0,0 +1,24 @@
+for i = 1, MIRRORTIMER_NUMTIMERS do
+    ---@type Texture
+    local border = _G[strconcat("MirrorTimer", i, "Border")]
+    border:Hide()
+end
+
+---@param frame Frame
+hooksecurefunc("MirrorTimerFrame_OnUpdate", function(frame)
+    if frame.paused then
+        return
+    end
+    local name = frame:GetName()
+    ---@type FontString
+    local label = _G[name .. "Text"]
+    local text = strsplit(" ", label:GetText())
+    ---@type StatusBar
+    local statusbar = _G[name .. "StatusBar"]
+    local _, maxValue = statusbar:GetMinMaxValues()
+    local currentValue = statusbar:GetValue()
+    ---@type ColorMixin
+    local color = currentValue > 30 and GREEN_FONT_COLOR or RED_FONT_COLOR
+    label:SetFormattedText("%s %s/%s", text, color:WrapTextInColorCode(SecondsToClock(currentValue)),
+            SecondsToClock(maxValue))
+end)
