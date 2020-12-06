26909a55b7c379cfc34c2bc53e12f3d773c347d2
czy211 <czy211@hotmail.com>
2020-12-06 20:33:22 +0800

    feat: 添加玩家增益和目标减益监控
    使用“/ab 按钮序号 法术id 内置cd”添加玩家增益;
    使用“/ad 按钮序号 法术id”添加目标减益。

--- src/WlkUI/AuraButtons.lua
+++ src/WlkUI/AuraButtons.lua
@@ -7,6 +7,224 @@ local spacing2 = 1
 local offset2 = size2 + spacing2
 local numBuffsPerRow = 15
 local numDebuffsPerRow = 8
+local size3 = 40
+local numberOfAuras = 8
+local width = size3 * 4
+local height = size3 * 2
+local addonName = ...
+local auras = { expirationTimes = {}, }
+
+---@type Frame
+local buffFrame = CreateFrame("Frame", "WlkBuffFrame", UIParent)
+---@type Frame
+local debuffFrame = CreateFrame("Frame", "WlkDebuffFrame", UIParent)
+---@type Frame
+local listener = CreateFrame("Frame")
+
+SLASH_ADD_BUFF1 = "/ab"
+SLASH_ADD_DEBUFF1 = "/ad"
+
+SlashCmdList["ADD_BUFF"] = function(arg)
+    local index, id, cooldown = strsplit(" ", arg, 3)
+    ---@type WlkAuraButton
+    local button = _G["WlkBuffButton" .. index]
+    if button then
+        local spec = GetSpecialization()
+        auras[spec].buffs[tonumber(index)] = tonumber(id)
+        auras[spec].cooldowns[tonumber(index)] = cooldown and tonumber(cooldown)
+        button.id = tonumber(id)
+        button.icon:SetTexture(GetSpellTexture(id))
+        button.duration = cooldown and tonumber(cooldown)
+    end
+end
+
+SlashCmdList["ADD_DEBUFF"] = function(arg)
+    local index, id = strsplit(" ", arg, 2)
+    ---@type WlkAuraButton
+    local button = _G["WlkDebuffButton" .. index]
+    if button then
+        local spec = GetSpecialization()
+        auras[spec].debuffs[tonumber(index)] = tonumber(id)
+        button.id = tonumber(id)
+        button.icon:SetTexture(GetSpellTexture(id))
+    end
+end
+
+buffFrame:SetSize(width, height)
+buffFrame:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOM", -122, 484)
+buffFrame:RegisterEvent("PLAYER_LOGIN")
+buffFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+buffFrame:RegisterEvent("UNIT_AURA")
+buffFrame:SetScript("OnEvent", function(_, event)
+    if event == "PLAYER_LOGIN" or event == "PLAYER_SPECIALIZATION_CHANGED" then
+        for i = 1, numberOfAuras do
+            local spec = GetSpecialization()
+            if not auras[spec] then
+                auras[spec] = { buffs = {}, debuffs = {}, cooldowns = {}, }
+            end
+            local id = auras[spec].buffs[i]
+            ---@type WlkAuraButton
+            local button = _G["WlkBuffButton" .. i]
+            button.id = id
+            button.icon:SetTexture(GetSpellTexture(id))
+            button.duration = auras[spec].cooldowns[i]
+            local expirationTime = auras.expirationTimes[id]
+            if button.duration and expirationTime and GetTime() <= expirationTime then
+                CooldownFrame_Set(button.cooldown, expirationTime - button.duration, button.duration,
+                        button.duration > 0, true)
+            end
+        end
+    elseif event == "UNIT_AURA" then
+        for i = 1, numberOfAuras do
+            _G["WlkBuffButton" .. i].show = nil
+        end
+        local index = 0
+        AuraUtil.ForEachAura("player", "HELPFUL", BUFF_MAX_DISPLAY, function(...)
+            local _, _, count, _, duration, expirationTime, _, _, _, id = ...
+            if id then
+                index = index + 1
+                for i = 1, numberOfAuras do
+                    ---@type WlkAuraButton
+                    local button = _G["WlkBuffButton" .. i]
+                    if button.id then
+                    end
+                    if id == button.id then
+                        if count > 1 then
+                            button.Count:SetText(count)
+                            button.Count:Show()
+                        else
+                            button.Count:Hide()
+                        end
+                        CooldownFrame_Set(button.cooldown, expirationTime - duration, duration, duration > 0, true)
+                        if button.duration and abs(button:GetAlpha() - 0.5) < 0.005 then
+                            auras.expirationTimes[id] = expirationTime - duration + button.duration
+                        end
+                        button:SetAlpha(1)
+                        button.show = true
+                    end
+                end
+            end
+            return index >= BUFF_MAX_DISPLAY
+        end)
+        for i = 1, numberOfAuras do
+            ---@type WlkAuraButton
+            local button = _G["WlkBuffButton" .. i]
+            if not button.show then
+                button:SetAlpha(0.5)
+                button.Count:Hide()
+                local expirationTime = auras.expirationTimes[button.id]
+                if button.duration and expirationTime and GetTime() <= expirationTime then
+                    CooldownFrame_Set(button.cooldown, expirationTime - button.duration, button.duration,
+                            button.duration > 0, true)
+                else
+                    CooldownFrame_Clear(button.cooldown)
+                end
+            end
+        end
+    end
+end)
+
+debuffFrame:SetSize(width, height)
+debuffFrame:SetPoint("BOTTOMLEFT", UIParent, "BOTTOM", 122, 484)
+debuffFrame:RegisterEvent("PLAYER_LOGIN")
+debuffFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+debuffFrame:RegisterEvent("UNIT_AURA")
+debuffFrame:SetScript("OnEvent", function(_, event)
+    if event == "PLAYER_LOGIN" or event == "PLAYER_SPECIALIZATION_CHANGED" then
+        for i = 1, numberOfAuras do
+            local spec = GetSpecialization()
+            if not auras[spec] then
+                auras[spec] = { buffs = {}, debuffs = {}, }
+            end
+            local id = auras[spec].debuffs[i]
+            ---@type WlkAuraButton
+            local button = _G["WlkDebuffButton" .. i]
+            button.id = id
+            button.icon:SetTexture(GetSpellTexture(id))
+        end
+    elseif event == "UNIT_AURA" then
+        for i = 1, numberOfAuras do
+            _G["WlkDebuffButton" .. i].show = nil
+        end
+        local index = 0
+        AuraUtil.ForEachAura("target", "HARMFUL|INCLUDE_NAME_PLATE_ONLY", DEBUFF_MAX_DISPLAY, function(...)
+            local _, _, count, _, duration, expirationTime, _, _, _, id = ...
+            if id then
+                index = index + 1
+                for i = 1, numberOfAuras do
+                    ---@type WlkAuraButton
+                    local button = _G["WlkDebuffButton" .. i]
+                    if id == button.id then
+                        if count > 1 then
+                            button.Count:SetText(count)
+                            button.Count:Show()
+                        else
+                            button.Count:Hide()
+                        end
+                        CooldownFrame_Set(button.cooldown, expirationTime - duration, duration, duration > 0, true)
+                        button:SetAlpha(1)
+                        button.show = true
+                    end
+                end
+            end
+            return index >= DEBUFF_MAX_DISPLAY
+        end)
+        for i = 1, numberOfAuras do
+            ---@type WlkAuraButton
+            local button = _G["WlkDebuffButton" .. i]
+            if not button.show then
+                button:SetAlpha(0.5)
+                button.Count:Hide()
+                CooldownFrame_Clear(button.cooldown)
+            end
+        end
+    end
+end)
+
+for i = 1, numberOfAuras do
+    ---@type ActionButtonTemplate
+    local button = CreateFrame("Button", "WlkBuffButton" .. i, buffFrame, "ActionButtonTemplate")
+    button:SetSize(size3, size3)
+    if i < 5 then
+        button:SetPoint("BOTTOMRIGHT", -size3 * (i - 1), 0)
+    else
+        button:SetPoint("TOPRIGHT", -size3 * (i - 5), 0)
+    end
+    button:SetAlpha(0.5)
+    button:EnableMouse(false)
+    button.cooldown:SetSwipeColor(0, 0, 0)
+    button.cooldown:SetReverse(true)
+    button.NormalTexture:SetTexture(nil)
+end
+
+for i = 1, numberOfAuras do
+    ---@type ActionButtonTemplate
+    local button = CreateFrame("Button", "WlkDebuffButton" .. i, debuffFrame, "ActionButtonTemplate")
+    button:SetSize(size3, size3)
+    if i < 5 then
+        button:SetPoint("BOTTOMLEFT", size3 * (i - 1), 0)
+    else
+        button:SetPoint("TOPLEFT", size3 * (i - 5), 0)
+    end
+    button:SetAlpha(0.5)
+    button:EnableMouse(false)
+    button.cooldown:SetSwipeColor(0, 0, 0)
+    button.cooldown:SetReverse(true)
+    button.NormalTexture:SetTexture(nil)
+end
+
+listener:RegisterEvent("ADDON_LOADED")
+listener:RegisterEvent("PLAYER_LOGIN")
+listener:SetScript("OnEvent", function(_, event, ...)
+    if event == "ADDON_LOADED" and ... == addonName then
+        listener:UnregisterEvent(event)
+        if WlkAuras then
+            auras = WlkAuras
+        else
+            WlkAuras = auras
+        end
+    end
+end)
 
 hooksecurefunc("CreateFrame", function(frameType, name, parent, template)
     if frameType == "Button" and name and parent == BuffFrame and (template == "BuffButtonTemplate"
@@ -116,3 +334,4 @@ end)
 
 ---@class WlkPlayerAuraButton:Button
 ---@class WlkTempEnchantButton:Button
+---@class WlkAuraButton:ActionButtonTemplate
