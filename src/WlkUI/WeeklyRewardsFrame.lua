fe68c94d02dd92a0c1a761192f8901e484c9f068
czy211 <czy211@hotmail.com>
2020-12-21 13:52:52 +0800

    fix: 修复首次登录时打开传奇钥石界面出现脚本错误的问题

--- src/WlkUI/WeeklyRewardsFrame.lua
+++ src/WlkUI/WeeklyRewardsFrame.lua
@@ -1,8 +1,26 @@
-if LoadAddOn("Blizzard_PVPUI") and LoadAddOn("Blizzard_ChallengesUI") and LoadAddOn("Blizzard_WeeklyRewards") then
-    PVPQueueFrame.HonorInset.CasualPanel.WeeklyChest:HookScript("OnMouseDown", function()
-        WeeklyRewardsFrame:Show()
-    end)
-    ChallengesFrame.WeeklyInfo.Child.WeeklyChest:HookScript("OnMouseDown", function()
-        WeeklyRewardsFrame:Show()
-    end)
-end
+local pvpLoaded, challengesLoaded
+
+---@type Frame
+local listener = CreateFrame("Frame")
+
+listener:RegisterEvent("ADDON_LOADED")
+listener:SetScript("OnEvent", function(_, event, ...)
+    if event == "ADDON_LOADED" then
+        if ... == "Blizzard_PVPUI" then
+            pvpLoaded = true
+        elseif ... == "Blizzard_ChallengesUI" then
+            challengesLoaded = true
+        end
+        if pvpLoaded and challengesLoaded then
+            listener:UnregisterEvent(event)
+            if LoadAddOn("Blizzard_WeeklyRewards") then
+                PVPQueueFrame.HonorInset.CasualPanel.WeeklyChest:HookScript("OnMouseDown", function()
+                    WeeklyRewardsFrame:Show()
+                end)
+                ChallengesFrame.WeeklyInfo.Child.WeeklyChest:HookScript("OnMouseDown", function()
+                    WeeklyRewardsFrame:Show()
+                end)
+            end
+        end
+    end
+end)
